eb6e05fc43d62ca28f810b258cc3ba68087c942e
ivanivanov-gitlab <ivan.ivanov@polymtl.ca>
2024-03-27 15:25:33 -0400

    integrate mock data to viz and added simulation to avoid bubble collisions

--- INF8808/src/app/bubble-bar-chart/bubble-bar-chart.component.ts
+++ INF8808/src/app/bubble-bar-chart/bubble-bar-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { data } from './data';
 
 @Component({
   selector: 'app-bubble-bar-chart',
@@ -7,20 +8,15 @@ import * as d3 from 'd3';
   styleUrls: ['./bubble-bar-chart.component.scss']
 })
 export class BubbleBarChartComponent implements OnInit {
-  private data = [
-    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
-    {"Framework": "React", "Stars": "150793", "Released": "2013"},
-    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
-    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
-    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
-  ];
+  private data = data;
   private svg: any;
   private margin = 50;
-  private width = 1300 - (this.margin * 2);
-  private height = 700 - (this.margin * 2);
+  private width = 750 - (this.margin * 2);
+  private height = 400 - (this.margin * 2);
 
   private xAxis: d3.ScaleBand<string> | undefined;
   private yAxis: d3.ScaleLinear<number, number> | undefined;
+  // TODO: Add xSubGroup (tp1)
 
   constructor() { }
 
@@ -31,10 +27,12 @@ export class BubbleBarChartComponent implements OnInit {
     this.yAxis = this.setYScale();
 
     this.createSvg();
-    this.drawBubble(this.data);
+    this.drawBubble();
     this.drawXAxis();
     this.drawYAxis();
-    // this.drawBars(this.data);
+
+    const simulation = this.getSimulation()
+    this.simulate(simulation)
   }
 
   private createSvg(): void {
@@ -51,31 +49,39 @@ export class BubbleBarChartComponent implements OnInit {
   private setXScale(): d3.ScaleBand<string> {
     return d3.scaleBand()
     .range([0, this.width])
-    .domain(this.data.map(d => d.Framework))
+    .domain(this.data.map(d => d.category))
     .padding(0.2);
   }
 
   private setYScale(): d3.ScaleLinear<number, number> {
     return d3.scaleLinear()
-    .domain([0, 200000])
+    .domain([0, d3.max(this.data, d => d.count) as number])
     .range([this.height, 0]);
   }
 
   // Drawings
 
-  private drawBubble(data: any[]): void {
+  private drawBubble(): void {
     this.svg.selectAll(".bubble")
     .data(data)
     .enter()
     .append("circle")
     .attr("class", "bubble")
-    .attr("cx", (d: any) => (this.xAxis as d3.ScaleBand<string>)(d.Framework) as number + (this.xAxis as d3.ScaleBand<string>).bandwidth() / 2)
-    .attr("cy", (d: any) => (this.yAxis as d3.ScaleLinear<number, number>)(d.Stars) as number)
-    .attr("r", (d: any) => d.Stars / 1000)
+    .attr("cx", (d: any) => {
+      d.x = (this.xAxis as d3.ScaleBand<string>)(d.category) as number + (this.xAxis as d3.ScaleBand<string>).bandwidth() / 2
+      return (this.xAxis as d3.ScaleBand<string>)(d.category) as number + (this.xAxis as d3.ScaleBand<string>).bandwidth() / 2
+    })
+    .attr("cy", (d: any) => {
+      // (this.yAxis as d3.ScaleLinear<number, number>)(d.count) as number
+      d.y = (this.yAxis as d3.ScaleLinear<number, number>)(400) as number
+      return (this.yAxis as d3.ScaleLinear<number, number>)(400) as number
+    })
+    .attr("r", (d: any) => d.count / 30)
     .attr("opacity", 0.7)
     .style("fill", "#d04a35")
     .on("mouseenter", function(event: any) {
       d3.select(event.target).attr("opacity", 1);
+      console.log(event.target.__data__)
     })
     .on("mouseleave", function(event: any) {
       d3.select(event.target).attr("opacity", 0.7);
@@ -98,23 +104,22 @@ export class BubbleBarChartComponent implements OnInit {
     .call(d3.axisLeft(this.yAxis as d3.ScaleLinear<number, number>));
   }
 
-  private drawBars(data: any[]): void {  
-    // Create and fill the bars
-    this.svg.selectAll("bars")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d: any) => (this.xAxis as d3.ScaleBand<string>)(d.Framework))
-    .attr("y", (d: any) => (this.yAxis as d3.ScaleLinear<number, number>)(d.Stars))
-    .attr("width", (this.xAxis as d3.ScaleBand<string>).bandwidth())
-    .attr("height", (d: any) => this.height - (this.yAxis as d3.ScaleLinear<number, number>)(d.Stars))
-    .attr("fill", "#d04a35")
-    .on("mouseenter", function(event: any) {
-      d3.select(event.target).attr("opacity", 1);
-    })
-    .on("mouseleave", function(event: any) {
-      d3.select(event.target).attr("opacity", 0.7);
-    });
+  // Simulation
+  private getSimulation() {
+    return d3.forceSimulation(this.data as d3.SimulationNodeDatum[])
+      .alphaDecay(0)
+      .velocityDecay(0.75)
+      .force('collision',
+        d3.forceCollide(20)
+          .strength(1)
+      );
   }
 
+  private simulate(simulation: any) {
+    simulation.on('tick', () => {
+        this.svg.selectAll('.bubble')
+          .attr('cx', (d: any) => d.x)
+          .attr('cy', (d: any) => d.y);
+      });
+  }
 }

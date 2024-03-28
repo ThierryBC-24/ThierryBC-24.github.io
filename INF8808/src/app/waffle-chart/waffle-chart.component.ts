52d9945795cf39cfda347f1004288c7449f08523
OliBigo <olivierbigonnesse258@gmail.com>
2024-03-28 13:38:52 -0400

    Merge with generated data

--- INF8808/src/app/waffle-chart/waffle-chart.component.ts
+++ INF8808/src/app/waffle-chart/waffle-chart.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
+type DataType = {
+  GROUPE_AGE: string;
+  NB_LESION: number;
+};
+
 @Component({
   selector: 'app-waffle-chart',
   templateUrl: './waffle-chart.component.html',
@@ -12,43 +17,32 @@ export class WaffleChartComponent implements OnInit {
   private svg: any;
   private width = window.innerWidth;
   private height = window.innerHeight;
-  private data = [
-    {
-      age: 'Moins de 20 ans',
-      value: 3200,
-    },
-    {
-      age: '20 ans à 24 ans',
-      value: 12800,
-    },
-    {
-      age: '25 ans à 34 ans',
-      value: 33600,
-    },
-    {
-      age: '35 ans à 44 ans',
-      value: 36800,
-    },
-    {
-      age: '45 ans à 54 ans',
-      value: 35200,
-    },
-    {
-      age: '55 ans à 64 ans',
-      value: 28800,
-    },
-    {
-      age: '65 ans ou plus',
-      value: 9600,
-    },
-  ];
+  private data: DataType[] = [];
   private totalValue = 0;
 
   ngOnInit(): void {
-    this.createSvg();
-    this.data.forEach((d) => (this.totalValue += d.value));
-    this.data.forEach((d, i) => this.drawWaffle(d, i));
-    this.drawLegend();
+    d3.text('/assets/data/Viz 3/groupe_age_lesion.csv').then((data) => {
+      let rowIndex = 0;
+      const parsedData: DataType[] = d3
+        .csvParseRows(data, (d: any) => {
+          rowIndex++;
+          if (rowIndex === 1) {
+            return null;
+          }
+          const columns = d[0].split(';');
+          return {
+            GROUPE_AGE: columns[0],
+            NB_LESION: +columns[1],
+          };
+        })
+        .filter((d) => d !== null) as DataType[];
+      this.data = parsedData;
+
+      this.createSvg();
+      this.data.forEach((d) => (this.totalValue += d.NB_LESION));
+      this.data.forEach((d, i) => this.drawWaffle(d, i));
+      this.drawLegend();
+    });
   }
 
   private createSvg(): void {
@@ -61,12 +55,37 @@ export class WaffleChartComponent implements OnInit {
       .attr('transform', `translate(0,0)`);
   }
 
-  private drawWaffle(data: any, index: number): void {
+  private addAgeText(chart: any, data: DataType, chartSize: number): void {
+    const ageText = data.GROUPE_AGE.replace(/(\d+)/g, '$1 ANS');
+    chart
+      .append('text')
+      .attr('x', chartSize / 2)
+      .attr('y', -12) // position the text 10px above the chart
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'central')
+      .attr('font-size', `${this.width * 0.013}px`)
+      .attr('fill', '#00408B')
+      .text(ageText);
+  }
+
+  private addValueText(chart: any, data: DataType, chartSize: number): void {
+    chart
+      .append('text')
+      .attr('x', chartSize / 2)
+      .attr('y', chartSize / 2)
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'central')
+      .attr('font-size', `${this.width * 0.02}px`)
+      .attr('fill', '#00408B')
+      .text(`${Math.round((100 * data.NB_LESION) / this.totalValue)}%`);
+  }
+
+  private drawWaffle(data: DataType, index: number): void {
     const rows = 10;
     const cols = 10;
-    const chartSpacing = this.width * 0.02; // adjust this value to change the spacing
+    const chartSpacing = this.width * 0.02;
     const chartSize =
-      (this.width - (this.data.length + 2) * chartSpacing) / this.data.length; // updated
+      (this.width - (this.data.length + 2) * chartSpacing) / this.data.length;
 
     const waffle = this.svg
       .append('g')
@@ -77,17 +96,6 @@ export class WaffleChartComponent implements OnInit {
         })`
       );
 
-    // Add age text above the chart
-    waffle
-      .append('text')
-      .attr('x', chartSize / 2)
-      .attr('y', -12) // position the text 10px above the chart
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'central')
-      .attr('font-size', `${this.width * 0.013}px`)
-      .attr('fill', '#00408B')
-      .text(data.age);
-
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         waffle
@@ -96,34 +104,35 @@ export class WaffleChartComponent implements OnInit {
           .attr('y', (rows - 1 - i) * (chartSize / rows)) // flip the y position
           .attr('width', chartSize / cols)
           .attr('height', chartSize / rows)
-          .attr('fill', (i * cols + j) * 1600 < data.value ? '#674FFA' : '#E7E7E7')
+          .attr(
+            'fill',
+            ((i * cols + j) * this.totalValue) / 100 < data.NB_LESION
+              ? '#674FFA'
+              : '#E7E7E7'
+          )
           .attr('stroke', '#fff')
           .attr('stroke-width', 3);
       }
     }
 
-    // Add text to the center of the chart
-    waffle
-      .append('text')
-      .attr('x', chartSize / 2)
-      .attr('y', chartSize / 2)
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'central')
-      .attr('font-size', `${this.width * 0.02}px`)
-      .attr('fill', '#00408B')
-      .text(`${(100 * data.value) / this.totalValue}%`);
+    this.addAgeText(waffle, data, chartSize);
+    this.addValueText(waffle, data, chartSize);
   }
 
   private drawLegend(): void {
-    const chartSize = (this.width - (this.data.length + 2) * this.width * 0.02) / this.data.length;
-    const legendY = (this.height - chartSize) / 2 + chartSize + this.height * 0.05; // adjust this value to change the padding
-  
-    const legend = this.svg.append('g')
+    const chartSize =
+      (this.width - (this.data.length + 2) * this.width * 0.02) /
+      this.data.length;
+    const legendY =
+      (this.height - chartSize) / 2 + chartSize + this.height * 0.05;
+
+    const legend = this.svg
+      .append('g')
       .attr('transform', `translate(${this.width / 2}, ${legendY})`)
       .attr('text-anchor', 'middle');
-  
-    // Add the text
-    legend.append('text')
+
+    legend
+      .append('text')
       .text(`1 carré = ${this.totalValue / 100} lésions`)
       .attr('font-size', '18px')
       .attr('fill', '#00408B');

8091fa3df77ac40e34d00b7081b0e07886730f46
Olivier Bigonnesse <olivier.bigonnesse@polymtl.ca>
2024-04-01 16:59:31 -0400

    added tool tips and such

--- INF8808/src/app/line-chart/line-chart.component.ts
+++ INF8808/src/app/line-chart/line-chart.component.ts
@@ -33,10 +33,16 @@ export class LineChartComponent implements OnInit {
     .domain(RISK_NAMES)
     .range(d3.schemeCategory10); // Use a categorical color scheme
 
+  private xScale: any;
+  private yScale: any;
+  private data: RisksData[] = [];
+
   constructor() {}
 
   ngOnInit(): void {
     this.initializeSvg();
+    this.drawLegend(RISK_NAMES);
+    this.drawBackground();
     this.drawChart();
   }
 
@@ -48,10 +54,10 @@ export class LineChartComponent implements OnInit {
     const containerHeight = container.offsetHeight;
 
     const margin = {
-      top: 0.25 * containerHeight,
-      right: 0.25 * containerWidth,
-      bottom: 0.25 * containerHeight,
-      left: 0.25 * containerWidth,
+      top: 0.1 * containerHeight,
+      right: 0.2 * containerWidth,
+      bottom: 0.1 * containerHeight,
+      left: 0.1 * containerWidth,
     };
 
     this.width = containerWidth - margin.left - margin.right;
@@ -70,6 +76,7 @@ export class LineChartComponent implements OnInit {
     d3.text('/assets/data/Viz 7/annee_risques.csv')
       .then((data) => {
         const parsedData: RisksData[] = this.parseCsvData(data);
+        this.data = parsedData;
 
         this.prepareData(parsedData);
 
@@ -91,7 +98,7 @@ export class LineChartComponent implements OnInit {
       .csvParseRows(data, (d: any) => {
         rowIndex++;
         if (rowIndex === 1) {
-          return null; // Skip the header row
+          return null;
         }
         const columns = d[0].split(';');
         return {
@@ -131,11 +138,12 @@ export class LineChartComponent implements OnInit {
     ];
     const yearRange = yearExtent[1] - yearExtent[0];
     const padding = yearRange * 0.05;
-
-    return d3
+    this.xScale = d3
       .scaleLinear()
       .domain([yearExtent[0] - padding, yearExtent[1] + padding])
       .range([0, this.width]);
+
+    return this.xScale;
   }
 
   private createYScale(parsedData: RisksData[]): any {
@@ -146,10 +154,12 @@ export class LineChartComponent implements OnInit {
     const minYValue = d3.min(valuesWithoutYear) || 0;
     const maxYValue = d3.max(valuesWithoutYear) || 0;
 
-    return d3
+    this.yScale = d3
       .scaleLinear()
       .domain([minYValue, maxYValue + 5])
       .range([this.height, 0]);
+
+    return this.yScale;
   }
 
   private drawLines(parsedData: RisksData[], xScale: any, yScale: any): void {
@@ -159,26 +169,39 @@ export class LineChartComponent implements OnInit {
         .x((d) => xScale(d.ANNEE))
         .y((d) => yScale(d[risk]));
 
-      this.svg
-        .append('path')
+      let g = this.svg.append('g');
+
+      g.append('path')
         .datum(parsedData)
         .attr('fill', 'none')
         .attr('stroke', () => this.colorScale(risk))
         .attr('stroke-width', 2)
-        .attr('d', line);
+        .attr('class', `line-${risk.replace(/ /g, '-')}`)
+        .attr('d', line)
+        .on('mouseover', () => {
+          this.handleHover(risk);
+        })
+        .on('mouseout', () => {
+          this.handleOut();
+        });
 
-      this.svg
-        .selectAll(`dot-${risk}`)
+      let self = this;
+      g.selectAll(`dot`)
         .data(parsedData)
         .enter()
         .append('circle')
         .attr('r', 5)
         .attr('cx', (d: RisksData) => xScale(d.ANNEE))
         .attr('cy', (d: RisksData) => yScale(d[risk]))
-        .style('fill', () => this.colorScale(risk));
+        .attr('class', `dot-${risk.replace(/ /g, '-')}`)
+        .style('fill', () => this.colorScale(risk))
+        .on('mouseover', function (this: SVGCircleElement) {
+          self.handleHover(risk, this);
+        })
+        .on('mouseout', () => {
+          this.handleOut();
+        });
     });
-
-    this.drawLegend(RISK_NAMES);
   }
 
   private drawAxes(xScale: any, yScale: any): void {
@@ -190,38 +213,20 @@ export class LineChartComponent implements OnInit {
           .axisBottom(xScale)
           .ticks(7)
           .tickFormat((value) => value.toString())
-      )
-  
-    this.svg
-      .append('g')
-      .call(d3.axisLeft(yScale).tickSize(-this.width).tickFormat((value) => `${value}%`))
-      .attr('stroke-opacity', 0.2)
-  }
+          .tickSize(0)
+          .tickPadding(5)
+      );
 
-  /*private drawAxes(xScale: any, yScale: any): void {
-    this.svg
-      .append('g')
-      .attr('transform', `translate(0,${this.height})`)
-      .call(
-        d3
-          .axisBottom(xScale)
-          .ticks(7)
-          .tickFormat((value) => value.toString())
-      ); // Use "d" format for whole numbers
-  
     this.svg
       .append('g')
       .call(
         d3
           .axisLeft(yScale)
-          .tickSize(-this.width) // This creates the horizontal lines across the plot area
-          .tickFormat(null) // This removes the tick labels
+          .tickSize(-this.width)
+          .tickFormat((value) => `${value}%`)
       )
-      .call((g: any) => g.select('.domain').remove()) // This removes the vertical line on y axis
-      .selectAll('.tick line') // select all the horizontal tick lines
-      .attr('stroke-opacity', 0.2) // make them less dark
-      .attr('stroke-dasharray', '2,2'); // make them dashed lines
-  }*/
+      .attr('stroke-opacity', 0.2);
+  }
 
   private drawLegend(risks: (keyof RisksData)[]): void {
     const legend = this.svg
@@ -234,7 +239,10 @@ export class LineChartComponent implements OnInit {
       .enter()
       .append('g')
       .attr('class', 'legend-item')
-      .attr('transform', (d: string, i: number) => `translate(0,${i * 20})`);
+      .attr('transform', (d: string, i: number) => `translate(0,${i * 20})`)
+      .each(function (this: any, d: string) {
+        d3.select(this).classed(d, true);
+      });
 
     legend
       .selectAll('.legend-item')
@@ -258,15 +266,89 @@ export class LineChartComponent implements OnInit {
       .attr('y', 0 - this.margin.left)
       .attr('x', 0 - this.height / 2)
       .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text('Percentage of Risks');
+      .style('text-anchor', 'middle');
 
     this.svg
       .append('text')
       .attr('x', this.width / 2)
       .attr('y', this.height + 40)
       .attr('text-anchor', 'middle')
-      .style('font-size', '16px')
-      .text('Line Chart of Risks Over Years');
+      .style('font-size', '16px');
+
+    this.svg
+      .append('text')
+      .attr('x', this.width / 2)
+      .attr('y', -20)
+      .attr('text-anchor', 'middle')
+      .style('font-size', '20px')
+      .style('font-weight', 'bold')
+      .text(
+        'Évolution du nombre de lésions liés aux risques prioritaires (2016-2022)'
+      );
+  }
+
+  private drawBackground(): void {
+    this.svg
+      .append('rect')
+      .attr('width', (4 * this.width) / 7)
+      .attr('height', this.height)
+      .attr('fill', 'silver')
+      .style('opacity', 0.5);
+  }
+
+  private handleHover(risk: keyof RisksData, circle?: SVGCircleElement): void {
+    d3.selectAll("circle[class*='dot']").style('opacity', 0.3);
+    d3.selectAll("path[class*='line']").style('opacity', 0.3);
+    d3.selectAll(`g[class^='legend-item']`).style('opacity', 0.3);
+    d3.selectAll(`circle[class*='dot-${risk.replace(/ /g, '-')}']`).style(
+      'opacity',
+      1
+    );
+    d3.selectAll(`path[class*='line-${risk.replace(/ /g, '-')}']`).style(
+      'opacity',
+      1
+    );
+    d3.selectAll(`g[class='legend-item ${risk}']`).style('opacity', 1);
+
+    if (circle) {
+      const data: RisksData = d3.select(circle).datum() as RisksData;
+      let g = d3.select(circle.parentNode as SVGGElement);
+
+      let nextYearData = this.data.find((d) => d.ANNEE === data.ANNEE + 1);
+
+      let tooltipText = g
+        .append('text')
+        .attr(
+          'x',
+          nextYearData ? circle.cx.baseVal.value : circle.cx.baseVal.value - 150
+        )
+        .attr('y', circle.cy.baseVal.value - 15)
+        .text(`Année: ${data.ANNEE}, Risque: ${data[risk].toFixed(2)}%`)
+        .attr('class', 'tooltip')
+        .style('fill', this.colorScale(risk) as string)
+        .style('font-weight', 'bold')
+        .style('font-size', '18px')
+        .style('opacity', '1');
+
+      let bbox = (tooltipText.node() as SVGTextElement).getBBox();
+
+      g.insert('rect', 'text')
+        .attr('x', bbox.x - 2)
+        .attr('y', bbox.y - 2)
+        .attr('width', bbox.width + 4)
+        .attr('height', bbox.height + 4)
+        .style('fill', 'white')
+        .style('opacity', 0.8)
+        .attr('class', 'tooltip');
+
+      tooltipText.raise();
+    }
+  }
+
+  private handleOut(): void {
+    d3.selectAll("circle[class*='dot']").style('opacity', 1);
+    d3.selectAll("path[class*='line']").style('opacity', 1);
+    d3.selectAll(`g[class^='legend-item']`).style('opacity', 1);
+    d3.selectAll('.tooltip').remove();
   }
 }

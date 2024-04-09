51605262a9dfc51f4e85b361a88d270346da516e
Malo457 <malostamourbilodeau@gmail.com>
2024-04-09 11:12:27 -0400

    MS: data dans un autre fichier

---
+++ INF8808/data/Viz 2/data_complete.ts
@@ -0,0 +1,197 @@
+export const data_viz2 = [
+    {
+        BodySeat: "REGION CRANIENNE, Y COMPRIS LE CRANE",
+        NLesions: 4203,
+        BodyPart: "Tête"
+    },
+    {
+        BodySeat: "OREILLE(S)",
+        NLesions: 11467,
+        BodyPart: "Tête"
+    },
+    {
+        BodySeat: "VISAGE",
+        NLesions: 2496,
+        BodyPart: "Tête"
+    },
+    {
+        BodySeat: "TETE NCA,TEMPE",
+        NLesions: 70,
+        BodyPart: "Tête"
+    },
+    {
+        BodySeat: "TETE,NP",
+        NLesions: 72,
+        BodyPart: "Tête"
+    },
+    {
+        BodySeat: "COU,SAUF SIEGE INTERNE",
+        NLesions: 37,
+        BodyPart: "Cou"
+    },
+    {
+        BodySeat: "SIEGE INT. DU COU NP,REG. CERVICALE",
+        NLesions: 2089,
+        BodyPart: "Cou"
+    },
+    {
+        BodySeat: "SIEGES INTERNES DU COU,NCA",
+        NLesions: 5,
+        BodyPart: "Cou"
+    },
+    {
+        BodySeat: "EPAULES",
+        NLesions: 6838,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "THORAX",
+        NLesions: 2035,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "DOS,COLONNE VERTEBRALE,MOELLE EPINI.",
+        NLesions: 19553,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "ABDOMEN",
+        NLesions: 224,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "REGION PELVIENNE",
+        NLesions: 867,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "TRONC,NCA",
+        NLesions: 12,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "TRONC,NP",
+        NLesions: 12,
+        BodyPart: "Tronc"
+    },
+    {
+        BodySeat: "BRAS",
+        NLesions: 4164,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "POIGNET(S)",
+        NLesions: 3181,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "MAIN(S),SAUF: DOIGT(S) SEULEMENT",
+        NLesions: 2919,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "DOIGT(S),ONGLE(S)",
+        NLesions: 7931,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "MEMBRES SUPERIEURS,NCA",
+        NLesions: 29,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "MEMBRES SUPERIEURS,NP",
+        NLesions: 24,
+        BodyPart: "Membres supérieurs"
+    },
+    {
+        BodySeat: "JAMBE(S)",
+        NLesions: 7311,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "CHEVILLE(S)",
+        NLesions: 4785,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "PIED(S),SAUF: ORTEIL(S) SEULEMENT",
+        NLesions: 2136,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "ORTEIL(S),ONGLE(S) D'ORTEIL(S)",
+        NLesions: 383,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "MEMBRES INFERIEURS,NCA",
+        NLesions: 22,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "MEMBRES INFERIEURS,NP",
+        NLesions: 30,
+        BodyPart: "Membres inférieurs"
+    },
+    {
+        BodySeat: "SYSTEMES CORPORELS",
+        NLesions: 69446,
+        BodyPart: "Systèmes corporels"
+    },
+    {
+        BodySeat: "SIEGES INTERNES MULTIPLES DU COU",
+        NLesions: 1,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "SIEGES MULTIPLES",
+        NLesions: 6777,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "SIEGES MULTIPLES DE LA TETE",
+        NLesions: 141,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "SIEGES MULTIPLES DU TRONC",
+        NLesions: 569,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "SIEGES MULTIPLES MEMBRES INFERIEURS",
+        NLesions: 652,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "SIEGES MULTIPLES MEMBRES SUPERIEURS",
+        NLesions: 813,
+        BodyPart: "Sièges multiples"
+    },
+    {
+        BodySeat: "APPAREILS PROSTHETIQUES (PROTHESES)",
+        NLesions: 628,
+        BodyPart: "Appareils prosthétiques"
+    },
+    {
+        BodySeat: "AUTRES SIEGES,NCA",
+        NLesions: 2,
+        BodyPart: "Autres"
+    },
+    {
+        BodySeat: "LARYNX",
+        NLesions: 2,
+        BodyPart: "Autres"
+    },
+    {
+        BodySeat: "NE PEUT ETRE CLASSE",
+        NLesions: 35,
+        BodyPart: "Autres"
+    },
+    {
+        BodySeat: "PHARYNX",
+        NLesions: 1,
+        BodyPart: "Autres"
+    }
+];
--- INF8808/src/app/viz2/viz2.component.ts
+++ INF8808/src/app/viz2/viz2.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as fs from "fs";
-import * as path from "path";
-import { parse } from 'csv-parse';
 import * as d3 from 'd3';
-
-interface Data {
-  bodyPart: string;
-  bodySeat: string;
-  nLesions: number;
-}
+import { data_viz2 } from 'data/Viz 2/data_complete';
 
 @Component({
   selector: 'app-viz2',
@@ -19,50 +11,10 @@ export class Viz2Component implements OnInit {
   private svg: any;
   private margin = 50;
   private marginLeft = 200;
-  private legendWidth = 300;
+  private legendWidth = 350;
   private width = 1400 - this.margin * 2;
   private height = 700 - this.margin * 2;
-  private data = [
-    {"BodySeat":"REGION CRANIENNE, Y COMPRIS LE CRANE","NLesions":4203, "BodyPart":"Tête"},
-    {"BodySeat":"OREILLE(S)","NLesions":11467, "BodyPart":"Tête"},
-    {"BodySeat":"VISAGE","NLesions":2496, "BodyPart":"Tête"},
-    {"BodySeat":"TETE NCA,TEMPE","NLesions":70, "BodyPart":"Tête"},
-    {"BodySeat":"TETE,NP","NLesions":72, "BodyPart":"Tête"},
-    {"BodySeat":"COU,SAUF SIEGE INTERNE","NLesions":37, "BodyPart":"Cou"},
-    {"BodySeat":"SIEGE INT. DU COU NP,REG. CERVICALE","NLesions":2089, "BodyPart":"Cou"},
-    {"BodySeat":"SIEGES INTERNES DU COU,NCA","NLesions":5, "BodyPart":"Cou"},
-    {"BodySeat":"EPAULES","NLesions":6838, "BodyPart":"Tronc"},
-    {"BodySeat":"THORAX","NLesions":2035, "BodyPart":"Tronc"},
-    {"BodySeat":"DOS,COLONNE VERTEBRALE,MOELLE EPINI.","NLesions":19553, "BodyPart":"Tronc"},
-    {"BodySeat":"ABDOMEN","NLesions":224, "BodyPart":"Tronc"},
-    {"BodySeat":"REGION PELVIENNE","NLesions":867, "BodyPart":"Tronc"},
-    {"BodySeat":"TRONC,NCA","NLesions":12, "BodyPart":"Tronc"},
-    {"BodySeat":"TRONC,NP","NLesions":12, "BodyPart":"Tronc"},
-    {"BodySeat":"BRAS","NLesions":4164, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"POIGNET(S)","NLesions":3181, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"MAIN(S),SAUF: DOIGT(S) SEULEMENT","NLesions":2919, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"DOIGT(S),ONGLE(S)","NLesions":7931, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"MEMBRES SUPERIEURS,NCA","NLesions":29, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"MEMBRES SUPERIEURS,NP","NLesions":24, "BodyPart":"Membres supérieurs"},
-    {"BodySeat":"JAMBE(S)","NLesions":7311, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"CHEVILLE(S)","NLesions":4785, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"PIED(S),SAUF: ORTEIL(S) SEULEMENT","NLesions":2136, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"ORTEIL(S),ONGLE(S) D'ORTEIL(S)","NLesions":383, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"MEMBRES INFERIEURS,NCA","NLesions":22, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"MEMBRES INFERIEURS,NP","NLesions":30, "BodyPart":"Membres inférieurs"},
-    {"BodySeat":"SYSTEMES CORPORELS","NLesions":69446, "BodyPart": "Systèmes corporels"},
-    {"BodySeat":"SIEGES INTERNES MULTIPLES DU COU","NLesions":1, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"SIEGES MULTIPLES","NLesions":6777, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"SIEGES MULTIPLES DE LA TETE","NLesions":141, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"SIEGES MULTIPLES DU TRONC","NLesions":569, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"SIEGES MULTIPLES MEMBRES INFERIEURS","NLesions":652, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"SIEGES MULTIPLES MEMBRES SUPERIEURS","NLesions":813, "BodyPart":"Sièges multiples"},
-    {"BodySeat":"APPAREILS PROSTHETIQUES (PROTHESES)","NLesions":628, "BodyPart":"Appareils prosthétiques"},
-    {"BodySeat":"AUTRES SIEGES,NCA","NLesions":2, "BodyPart":"Autres" },
-    {"BodySeat":"LARYNX","NLesions":2, "BodyPart":"Autres" },
-    {"BodySeat":"NE PEUT ETRE CLASSE","NLesions":35, "BodyPart":"Autres" },
-    {"BodySeat":"PHARYNX","NLesions":1, "BodyPart":"Autres" }
-  ]
+  private data = data_viz2;
 
   constructor() { }
 
@@ -84,9 +36,7 @@ export class Viz2Component implements OnInit {
   private formatData(data: any[]): any {
     const lesions = data.map(item => item.NLesions);
     const sum = lesions.reduce((acc, curr) => acc + curr, 0);
-    data.forEach(item => {
-      item.percent = item.NLesions/sum;
-    });
+    data.forEach(item => item.percent = item.NLesions/sum);
 
     const grouped = data.reduce((groups, item) => {
       (groups[item.BodyPart] ||= []).push(item);
@@ -109,7 +59,7 @@ export class Viz2Component implements OnInit {
     const paddingBodyParts = 20;
 
     const x = d3.scaleLinear()
-      .domain([0, scaleBreak, scaleBreak, 0.6])
+      .domain([0, scaleBreak, scaleBreak, 0.5])
       .range([0, this.width - 400 - this.legendWidth, this.width - 400 - this.legendWidth, this.width - this.legendWidth]);
 
     const y = d3.scaleBand()
